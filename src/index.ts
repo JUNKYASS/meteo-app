683c9e0e9b0dc6681849ee56f0b0a23b541e4335
Ruslan Mikhaylichenko <h3ymaybeqq@gmail.com>
2022-02-20 22:49:31 +0300

    dbservices class has been added

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,33 @@ import { registerServicerWorker } from './utils';
 
 window.addEventListener('load', () => {
   registerServicerWorker();
+  const db = new DBServices();
 });
+
+
+
+class DBServices {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    const DBConnect = indexedDB.open('meteoDB', 3);
+
+    DBConnect.addEventListener('upgradeneeded', () => {
+      const db = DBConnect.result;
+
+      if (!db.objectStoreNames.contains('temperature')) {
+        db.createObjectStore('temperature', { keyPath: 'id' });
+      }
+    });
+
+    DBConnect.addEventListener('error', (e) => {
+      console.error('Error:', DBConnect.error);
+    });
+
+    DBConnect.addEventListener('success', (e) => {
+
+    });
+  }
+}
