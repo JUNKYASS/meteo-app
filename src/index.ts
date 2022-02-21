8fb23071121300211f6ef2d2d1f9f71e9c53ab0a
Ruslan Mikhaylichenko <h3ymaybeqq@gmail.com>
2022-02-21 09:55:44 +0300

    trouble with indexeddb - need to find out what's the problem

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,164 @@
 import { registerServiceWorker } from './utils';
 import * as jsonData from './data/precipitation.json';
 
+const addHandler = () => {
+  console.log('Row has been added (by manual callback!)');
+};
+
 window.addEventListener('load', () => {
   registerServiceWorker();
 
-  const db = new DBServices();
+  // const db = new DBServices();
+  // db.add('temperature', 'readwrite', data, addHandler);
+
+  const data = [
+    {
+      date: '2019-12-04',
+      t: '25',
+    },
+    {
+      date: '2019-12-06',
+      t: '22',
+    },
+    {
+      date: '2019-12-05',
+      t: '19',
+    }
+  ];
+
+
+  const DBConnect = indexedDB.open('meteo', 2);
+
+  DBConnect.addEventListener('upgradeneeded', () => {
+    const db = DBConnect.result;
+
+    if (!db.objectStoreNames.contains('temperature')) {
+      const temperatureStore = db.createObjectStore('temperature', { keyPath: 'id', autoIncrement: true }); // Create storage for temperature
+      const index = temperatureStore.createIndex('date_index', 'date'); // Create index for the date field
+    }
+
+    if (!db.objectStoreNames.contains('precipitation')) {
+      const precipitationStore = db.createObjectStore('precipitation', { keyPath: 'id', autoIncrement: true }); // Create storage for precipitation
+      const index = precipitationStore.createIndex('date_index', 'date'); // Create index for the date field
+    }
+  });
+
+  DBConnect.addEventListener('error', () => {
+    console.error('Error:', DBConnect.error);
+  });
+
+  DBConnect.addEventListener('success', () => {
+    console.log('DB is ready...');
+
+    const newDb = DBConnect.result;
+
+    // add('temperature', 'readwrite', data, addHandler, DBConnect.result);
+    const transaction = newDb.transaction('temperature', 'readwrite');
+    const store = transaction.objectStore('temperature');
+    let request = store.add(data); // Insert data to DB
+
+    console.log('[DDB]:', request);
+
+    request.addEventListener('success', () => {
+      console.log("Row just added: ", request.result);
+      // callback(); // Execute given callback
+    });
+
+    request.addEventListener('error', () => {
+      console.log("Error: ", request.error);
+    });
+  });
+
+  function add(storeName: string, flag: 'readonly' | 'readwrite' = 'readonly', data: any, callback: () => any, db: any) {
+    const transaction = db.transaction(storeName, flag);
+    const store = transaction.objectStore(storeName);
+    let request = store.add(data); // Insert data to DB
+
+    console.log('[DDB]:', request);
+
+    request.addEventListener('success', () => {
+      console.log("Row just added: ", request.result);
+      callback(); // Execute given callback
+    });
+
+    request.addEventListener('error', () => {
+      console.log("Error: ", request.error);
+    });
+  }
+
+  // const add = (storeName: string, flag: 'readonly' | 'readwrite' = 'readonly', data: any, callback: () => any, db: any) => {
+  //   const transaction = db.transaction(storeName, flag);
+  //   const store = transaction.objectStore(storeName);
+  //   let request = store.add(data); // Insert data to DB
+
+  //   console.log('[DDB]:', request);
+
+  //   request.addEventListener('success', () => {
+  //     console.log("Row just added: ", request.result);
+  //     callback(); // Execute given callback
+  //   });
+
+  //   request.addEventListener('error', () => {
+  //     console.log("Error: ", request.error);
+  //   });
+  // }
 });
 
+// interface IDBServices {
+//   DBConnect: IDBOpenDBRequest | undefined
+//   DBConnectResult: IDBDatabase | undefined
+// }
 
+// class DBServices implements IDBServices {
+//   DBConnect = undefined;
+//   DBConnectResult = undefined;
 
-class DBServices {
-  constructor() {
-    this.init();
-  }
+//   constructor() {
+//     this.init();
+//   }
 
-  init() {
-    const DBConnect = indexedDB.open('meteoDB', 3);
+  // init() {
+  //   this.DBConnect = indexedDB.open('meteoDB', 8);
 
-    DBConnect.addEventListener('upgradeneeded', () => {
-      const db = DBConnect.result;
+  //   this.DBConnect.addEventListener('upgradeneeded', () => {
+  //     const db = this.DBConnect.result;
 
-      if (!db.objectStoreNames.contains('temperature')) {
-        db.createObjectStore('temperature', { keyPath: 'id' });
-      }
-    });
+  //     if (!db.objectStoreNames.contains('temperature')) {
+  //       const temperatureStore = db.createObjectStore('temperature', { keyPath: 'id' }); // Create storage for temperature
+  //       const index = temperatureStore.createIndex('date_index', 'date'); // Create index for the date field
+  //     }
 
-    DBConnect.addEventListener('error', (e) => {
-      console.error('Error:', DBConnect.error);
-    });
+  //     if (!db.objectStoreNames.contains('precipitation')) {
+  //       const precipitationStore = db.createObjectStore('precipitation', { keyPath: 'id' }); // Create storage for precipitation
+  //       const index = precipitationStore.createIndex('date_index', 'date'); // Create index for the date field
+  //     }
+  //   });
 
-    DBConnect.addEventListener('success', (e) => {
+  //   this.DBConnect.addEventListener('error', () => {
+  //     console.error('Error:', this.DBConnect.error);
+  //   });
 
-    });
-  }
-}
+  //   this.DBConnect.addEventListener('success', () => {
+  //     console.log('DB is ready...');
+
+  //     this.add('temperature', 'readwrite', data, addHandler);
+  //   });
+  // }
+
+//   add(storeName: string, flag: 'readonly' | 'readwrite' = 'readonly', data: any, callback: () => any) {
+//     const transaction = this.DBConnect.result.transaction(storeName, flag);
+//     const store = transaction.objectStore(storeName);
+//     let request = store.add(data); // Insert data to DB
+
+//     console.log('[DDB]:', request);
+
+//     request.addEventListener('success', () => {
+//       console.log("Row just added: ", request.result);
+//       callback(); // Execute given callback
+//     });
+
+//     request.addEventListener('error', () => {
+//       console.log("Error: ", request.error);
+//     });
+//   }
+// }
